d078ef07c1023dc0c2aa0437b18eb546d7c174fb
Matdomingues08 <matheusedu.theuso@gmail.com>
2025-10-27 13:36:06 +0000

    finish model with logistic regression and gerenating joblib file

--- modelo_treinamentoo.py
+++ modelo_treinamentoo.py
@@ -24,6 +24,79 @@ def carregador_dados(caminho_arquivo='historicoAcademico.csv'):
 
         return None
     
-
 dados = carregador_dados()
-print(dados)
+
+
+#--------- ETAPA 02 : PREPARAÇÃO E DIVISÃO DE DADOS
+#Definicção de x (features) e y (target)
+
+if dados is not None:
+    print(f"\nTotal de regsitros carregados{len(dados)}")
+    print("iniciando o pipeline de treinamento")
+
+    TARGET_COLUMN = 'Status_Final'
+    #etapa 2.1 = definição das features e target 
+    try:
+        x = dados.drop (TARGET_COLUMN, axis=1)
+        y = dados[TARGET_COLUMN]
+        print(f"Features (x) definidaas: {list(x.columns)}")
+        print(f"Features (y) definidas: {TARGET_COLUMN}")
+
+    except KeyError:
+
+        print(f"\n----Erro Critico----")
+        print(f"A coluna {TARGET_COLUMN} não foi encontrado no CSV")
+        print(f"Colunas disponiveis: {list(dados.columns)}")
+        print(f"Por favor, ajuste a variavel 'TARGET_COLUMN' e tente novamente")
+        #se o target não for encontrado irá encerrar o script!
+        exit()
+
+    #etapa 2.2 - divisão entre treino e teste
+    print("\n----- Dividindo dados e em treino e teste -----")
+
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(x, y,
+        test_size=0.2,  #20%% dos dados serão utilizados para teste
+        random_state=42, #garantir a reprodutibidade
+        stratify=y       #Manter a proporção aprovados e reprovados
+    )
+    print(f"Dados de treino: {len(X_train)}) | Dados de teste: {len(X_test)}")
+
+    # etapa 3: criação da pipeçine de ML
+
+    print("\nCriando a pipeline da ML...-----")
+    pipeline_model = pipeline.Pipeline([  
+        ('scaler', preprocessing.StandardScaler()),
+        ('model', linear_model.LogisticRegression(random_state=42))
+    ])
+    #etapa 04 
+    print("\n---------TREINAEMNTO DE MODELO...-----------") 
+    #treina a pipeline com os dados de treino
+    pipeline_model.fit(X_train, y_train)
+
+    print("modelo treinado. Avaliando com os dados de teste")
+    Y_pred = pipeline_model.predict(X_test)
+
+    #AVALIAÇÃO D DESEMPENHO
+
+    accuracy = metrics.accuracy_score(y_test, Y_pred)
+    report = metrics.classification_report(y_test, Y_pred)
+
+    print("\n-----Relatorio de avaliação geral------")
+    print(f"Acuráia GEral: {accuracy * 100:2f}%")
+    print("\nRelátorio de classificação detalhado")
+    print(report)
+
+
+    #Etapa 5: salvando o modelo
+    model_filename = 'modelo_previsao_desempenho.joblib'
+
+    print(f"\nsalvando o pipeline treinado em.. {model_filename}")
+    joblib.dump(pipeline_model, model_filename)
+
+
+    print("Processo concluido com sucesso")
+
+    print(" arquivo '{model_filename}' está para ser utilizado")
+
+else:
+    print ("O pipeline não pode continuar pois os dados não dorma carregados!")
